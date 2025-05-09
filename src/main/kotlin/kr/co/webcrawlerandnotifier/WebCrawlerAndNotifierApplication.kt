d1d56d710e5ea4958b7c463741e464d6922de315
Nate <youngmini90@naver.com>
2025-05-09 11:30:17 +0900

    Init

---
+++ src/main/kotlin/kr/co/webcrawlerandnotifier/WebCrawlerAndNotifierApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.webcrawlerandnotifier
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebCrawlerAndNotifierApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebCrawlerAndNotifierApplication>(*args)
+}
